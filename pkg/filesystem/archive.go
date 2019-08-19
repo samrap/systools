f52ed36f827681f24c85824bf85f7a9d01d926e8
Sam Rapaport <me@samrapdev.com>
2019-08-18 22:44:13 -0700

    Support symbolic links for tarballs.

--- pkg/filesystem/archive.go
+++ pkg/filesystem/archive.go
@@ -12,50 +12,78 @@ import (
 // CreateTarball takes a source directory and walks the directory recursively,
 // constructing a tarball stored in `targetDir`. It returns `tarpath`, the
 // full path to the created tarfile and `err` if the archival fails.
-func CreateTarball(src, targetDir string) (tarpath string, err error) {
+func CreateTarball(source, targetDir string) (tarPath string, err error) {
+	// baseName is the base directory of `source`, which must be included in the
+	// file name of the headers written to the tarball. For example, if the
+	// source is directory `/foo/bar`, and `bar` contains several files
+	// which will be put in the archive, their header's name field
+	// must include their base directory, `bar`, such that the
+	// header contains the entire path relative to `bar`.
+	//
+	// If `source` is not a directory, then `baseName` should be an empty string.
+	var baseName string
+
 	// Get file info for the source file.
-	srcInfo, err := os.Stat(src)
+	sourceInfo, err := os.Stat(source)
 	if err != nil {
-		return tarpath, err
+		return tarPath, err
+	}
+
+	// We only need to specify the base directory if `source` is a directory.
+	if sourceInfo.IsDir() {
+		baseName = filepath.Base(source)
 	}
 
 	// Create a tar file to write to.
-	tarpath = filepath.Join(targetDir, fmt.Sprintf("%s.tar", srcInfo.Name()))
-	tarfile, err := os.Create(tarpath)
+	tarPath = filepath.Join(targetDir, fmt.Sprintf("%s.tar", sourceInfo.Name()))
+	tarfile, err := os.Create(tarPath)
 	if err != nil {
-		return tarpath, err
+		return tarPath, err
 	}
 	defer tarfile.Close()
 
+	// Create a tarball to write to.
 	tarball := tar.NewWriter(tarfile)
 	defer tarball.Close()
 
-	var baseDir string
-	if srcInfo.IsDir() {
-		baseDir = filepath.Base(src)
-	}
+	return tarPath, filepath.Walk(source, func(path string, info os.FileInfo, err error) error {
+		// link is the destination that path points to if it is a symbolic link.
+		var link string
 
-	return tarpath, filepath.Walk(src, func(path string, info os.FileInfo, err error) error {
-		if err != nil {
-			return err
-		}
-		header, err := tar.FileInfoHeader(info, info.Name())
 		if err != nil {
 			return err
 		}
 
-		if baseDir != "" {
-			header.Name = filepath.Join(baseDir, strings.TrimPrefix(path, src))
+		// If we are dealing with a symbolic link, then we need to get the path
+		// that it points to in order to write a valid header for this file.
+		if info.Mode()&os.ModeSymlink != 0 {
+			if link, err = os.Readlink(path); err != nil {
+				return err
+			}
 		}
 
+		// Create the header.
+		header, err := tar.FileInfoHeader(info, link)
+		if err != nil {
+			return err
+		}
+
+		// We need to include the base name of source if it is a directory.
+		if baseName != "" {
+			header.Name = filepath.Join(baseName, strings.TrimPrefix(path, source))
+		}
+
+		// Write the header.
 		if err := tarball.WriteHeader(header); err != nil {
 			return err
 		}
 
-		if info.IsDir() {
+		// If we are dealing with a non-regular file, our work is done here.
+		if !info.Mode().IsRegular() {
 			return nil
 		}
 
+		// For regular files, we will copy their contents into the tarball.
 		file, err := os.Open(path)
 		if err != nil {
 			return err
@@ -66,8 +94,8 @@ func CreateTarball(src, targetDir string) (tarpath string, err error) {
 	})
 }
 
-// ExtractTarball takes a reader containing a tarball and attempts to extract
-// it into `targetDir`.
+// ExtractTarball takes a reader containing tarball bytes and attempts to extract
+// the archive into `targetDir`.
 func ExtractTarball(reader io.Reader, targetDir string) error {
 	tarReader := tar.NewReader(reader)
 
@@ -81,13 +109,25 @@ func ExtractTarball(reader io.Reader, targetDir string) error {
 
 		path := filepath.Join(targetDir, header.Name)
 		info := header.FileInfo()
+
 		if info.IsDir() {
+			// If the file is a directory, we will simply create the directory
+			// with the original permissions.
 			if err = os.MkdirAll(path, info.Mode()); err != nil {
+				return err
+			}
+			continue
+		} else if info.Mode()&os.ModeSymlink != 0 {
+			// If the file is a symlink, we will simply create the link using
+			// the link name stored in the header.
+			if err = os.Symlink(header.Linkname, path); err != nil {
 				return err
 			}
 			continue
 		}
 
+		// Finally, if we are dealing with a regular file we will copy the
+		// contents from the tar reader into a newly created file.
 		file, err := os.OpenFile(path, os.O_CREATE|os.O_TRUNC|os.O_WRONLY, info.Mode())
 		if err != nil {
 			return err
